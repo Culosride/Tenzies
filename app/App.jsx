eb1488069e5073221b9e10a94bfc59263a05e56b
Andrea Spiga <drespriz@gmail.com>
2022-09-07 17:55:20 +0200

    die component

--- app/App.jsx
+++ app/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Die from '../app/components/Die.jsx'
 
 export default function App() {
     return (
-        <div className='wrapper'>
-
-        </div>
+      <div className='container-main'>
+        <Die value=""/>
+      </div>
     )
 }
