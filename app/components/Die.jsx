ae786d071093ff91cf5586c23ba338198ec2074a
Andrea Spiga <drespriz@gmail.com>
2022-09-13 11:28:19 +0200

    substitutes numbers values with die faces

--- app/components/Die.jsx
+++ app/components/Die.jsx
@@ -1,11 +1,17 @@
 export default function Die(props) {
   const styles = {
-    backgroundColor: props.held ? "#f33dc2" : "white"
+    backgroundColor: props.held ? "#f33dc2" : "white",
+    backgroundPositionX: props.value === 6 && "50px" ||
+    props.value === 5 && "102px" ||
+    props.value === 4 && "153.5px" ||
+    props.value === 3 && "205px" ||
+    props.value === 2 && "-51.7px" ||
+    props.value === 1 && "308.5px"
 }
 
 return (
       <div onClick={props.holdDice} className="die" style={styles}>
-        <h3 className="number">{props.value}</h3>
+        <h3 className="number"></h3>
       </div>
   )
 }
